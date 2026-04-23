1a25373420f66f542e268bba6fa90e8f81b779e2
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-04-23 16:50:56 -0400

    Initial Commit

---
+++ AppleIDTest/AppleIDTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleIDTestApp.swift
+//  AppleIDTest
+//
+//  Created by Lidiia Diachkovskaia on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct AppleIDTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
